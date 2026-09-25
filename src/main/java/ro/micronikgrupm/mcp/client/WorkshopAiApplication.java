6404fbfd35ccfeded8fbdc0b05590fdf11c634ac
George Sand <iqgeo@yahoo.com>
2026-09-25 18:08:28 +0300

    add init

--- src/main/java/ro/micronikgrupm/mcp/client/WorkshopAiApplication.java
+++ src/main/java/ro/micronikgrupm/mcp/client/WorkshopAiApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /** Auto-generated documentation. */
 @SpringBootApplication
 public class WorkshopAiApplication {
+
   public static void main(String[] args) {
     SpringApplication.run(WorkshopAiApplication.class, args);
   }
+
 }
